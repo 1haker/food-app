e3671aa85ab8dc6c3c8a3e12c20cad0633f5bbd0
1haker <nowakj917@gmail.com>
2020-06-16 16:45:46 +0200

    sticky

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
 const toTop = document.querySelector('.to-top');
 
 window.addEventListener('scroll', () => {
+  var header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 0);
+
   if (window.pageYOffset > 100) {
     toTop.classList.add('active');
   } else {
     toTop.classList.remove('active');
   }
 });
+
+// window.addEventListener('scroll'),
+//   () => {
+//     var header = document.querySelector('header');
+//     header.classList.toggle('sticky', window.scrollY > 0);
+//   };
+
+// window.onscroll = function () {
+//   myFunction();
+// };
+
+// var navbar = document.getElementById('navbar');
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add('sticky');
+//   } else {
+//     navbar.classList.remove('sticky');
+//   }
+// }
